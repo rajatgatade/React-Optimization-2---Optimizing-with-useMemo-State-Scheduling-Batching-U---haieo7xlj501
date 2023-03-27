7e537270612ba7ff0f08bb85209b74a14ccd6693
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-03-27 14:58:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,37 @@ const App = () => {
 const OptimizeTheOperation = ({ onClick }) => {
   const [number, setNumber] = useState(10000);
 
-  const prime = primeNumber(number);
+  const prime = useMemo(() => primeNumber(number), [number]);
 
   const submitHandler = (event) => {
     event.preventDefault();
     setNumber(Number(event.target.num.value));
   };
+
+  return (
+    <div>
+      <br />
+      Enter the number:
+      <form onSubmit={submitHandler}>
+        <input id="num" />
+        <button id="submit" type="submit">
+          Click me 
+        </button>
+      </form>
+      <br />
+      <div className="width">
+        Result of expensive operation:
+        <ul>
+          {prime.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <br />
+    </div>
+  );
+};
+
 
   return (
     <div>
